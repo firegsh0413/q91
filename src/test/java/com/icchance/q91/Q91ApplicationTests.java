e8375685db9d6ad7d989d7bb326ee5edf976e2a2
6687353 <6687353@icchance.com>
2023-07-19 17:53:46 +0800

    test

---
+++ src/test/java/com/icchance/q91/Q91ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icchance.q91;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q91ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
