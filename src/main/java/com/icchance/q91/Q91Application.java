0bd91c8316570c907210311619e50b316cd1e9c2
6687353 <6687353@icchance.com>
2023-07-19 17:30:04 +0800

    test

---
+++ src/main/java/com/icchance/q91/Q91Application.java
@@ -0,0 +1,11 @@
+package com.icchance.q91;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q91Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Q91Application.class, args);
+    }
+}
---
+++ src/main/java/com/icchance/q91/controller/OrderController.java
@@ -0,0 +1,9 @@
+package com.icchance.q91.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class OrderController extends BaseController {
+}
